1ac3f997702b2e47580bf884f3a6b819f555b7df
Orkhan Zeynalli <zeynalli169@gmail.com>
2022-03-25 16:35:59 +0400

    container name bugfix

--- postgres/deploy.sh
+++ postgres/deploy.sh
@@ -2,10 +2,10 @@ echo "load env.."
 . env.sh
 bash ../remove.sh "$CONTAINER_NAME"
 sudo docker run -d \
-    --name postgres-dev \
-    -e POSTGRES_PASSWORD="$POSTGRES_PASSWORD" \
-    -e POSTGRES_USER="$POSTGRES_USER" \
-    --network="$NETWORK" \
-    -v "$DATA_PATH":/var/lib/postgresql/data \
-    --restart unless-stopped \
-    postgres:"$VERSION"
+  --name "$CONTAINER_NAME" \
+  -e POSTGRES_PASSWORD="$POSTGRES_PASSWORD" \
+  -e POSTGRES_USER="$POSTGRES_USER" \
+  --network="$NETWORK" \
+  -v "$DATA_PATH":/var/lib/postgresql/data \
+  --restart unless-stopped \
+  postgres:"$VERSION"
