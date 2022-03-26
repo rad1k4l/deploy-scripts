fbd9ab211fc75e76938a2f1ed176af0ce47cec1f
rad1k4l <zeynalli169@gmail.com>
2022-03-26 22:46:43 +0400

    added drone runner

---
+++ drone-ci/docker-runner/deploy.sh
@@ -0,0 +1,14 @@
+echo "load env.."
+. env.sh
+bash ../../remove.sh "$CONTAINER_NAME"
+docker run --detach \
+  --volume="$DOCKER_SOCKET":/var/run/docker.sock \
+  --env=DRONE_RPC_PROTO="$SERVER_PROTO" \
+  --env=DRONE_RPC_HOST="$SERVER_HOST" \
+  --env=DRONE_RPC_SECRET="$RPC_SECRET" \
+  --env=DRONE_RUNNER_CAPACITY=2 \
+  --env=DRONE_RUNNER_NAME="main-ovh-runner" \
+  --restart unless-stopped \
+  --network="$NETWORK" \
+  --name "$CONTAINER_NAME" \
+  drone/drone-runner-docker:"$VERSION"
---
+++ drone-ci/docker-runner/env.example.sh
@@ -0,0 +1,7 @@
+CONTAINER_NAME="drone-ci-github-runner"
+DOCKER_SOCKET="/var/run/docker.sock"
+RPC_SECRET="super-duper-secret"
+SERVER_HOST="drone-ci-github-server"
+SERVER_PROTO="http"
+NETWORK="main"
+VERSION="1.8.0"
