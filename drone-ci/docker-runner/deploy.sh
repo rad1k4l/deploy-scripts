e58e90af5dc0d3e5cc63084e25da0dd8746c80e2
rad1k4l <zeynalli169@gmail.com>
2022-03-27 18:45:33 +0400

    add drone container networks

--- drone-ci/docker-runner/deploy.sh
+++ drone-ci/docker-runner/deploy.sh
@@ -7,6 +7,7 @@ docker run --detach \
   --env=DRONE_RPC_HOST="$SERVER_HOST" \
   --env=DRONE_RPC_SECRET="$RPC_SECRET" \
   --env=DRONE_RUNNER_CAPACITY=2 \
+  --env=DRONE_RUNNER_NETWORKS="$NETWORK" \
   --env=DRONE_RUNNER_NAME="main-ovh-runner" \
   --restart unless-stopped \
   --network="$NETWORK" \
