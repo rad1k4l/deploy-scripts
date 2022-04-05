1ec43da107523690d52d186666f94dfd1b71c9a9
Farhad <62416812+Xobolov@users.noreply.github.com>
2022-04-05 11:39:56 +0400

    fix

--- nginx/deploy.sh
+++ nginx/deploy.sh
@@ -5,8 +5,8 @@ sudo docker run --detach \
   --name "$CONTAINER_NAME" \
   --publish 80:80 \
   --network="$NETWORK" \
-  -volume "$WEB_PATH":/usr/share/nginx/html \
-  -volume "$CONFIG_PATH":/etc/nginx/conf.d \
+  --volume "$WEB_PATH":/usr/share/nginx/html \
+  --volume "$CONFIG_PATH":/etc/nginx/conf.d \
   --restart unless-stopped \
   nginx:"$VERSION"
 
