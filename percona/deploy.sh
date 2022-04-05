fe9dea0d6b79c962eee3b468f7014e12733d8f29
Khanbala Rashidov <50279392+KhanbalaRashidov@users.noreply.github.com>
2022-04-05 11:37:45 +0400

    Update deploy.sh

--- percona/deploy.sh
+++ percona/deploy.sh
@@ -3,9 +3,9 @@ echo "load env.."
 bash ../remove.sh "$CONTAINER_NAME"
 sudo docker run -d \
   --name "$CONTAINER_NAME" \
-  -env MYSQL_ROOT_PASSWORD="$POSTGRES_PASSWORD" \
+  -env=MYSQL_ROOT_PASSWORD="$POSTGRES_PASSWORD" \
   --network="$NETWORK" \
-  -volume "$DATA_PATH":/var/lib/mysql \
-  -volume "$CONFIG_PATH":/etc/mysql \
+  --volume "$DATA_PATH":/var/lib/mysql \
+  --volume "$CONFIG_PATH":/etc/mysql \
   --restart unless-stopped \
   percona/percona-server:"$VERSION"
