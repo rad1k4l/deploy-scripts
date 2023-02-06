c745de40d485141b8e7b62fbff3111fcbe5d3888
Khanbala Rashidov <50279392+KhanbalaRashidov@users.noreply.github.com>
2023-02-07 00:19:22 +0300

    Update deploy.sh

--- nodejs/deploy.sh
+++ nodejs/deploy.sh
@@ -4,8 +4,8 @@ bash ../remove.sh "$CONTAINER_NAME"
 sudo docker run --detach \
     --name "$CONTAINER_NAME" \
     --publish 3000:3000 \
-    --env="$Node_ENV" \
-    --env="$APP" \
-    --network="$NETWORK" \
+    --env NODE_ENV="$NODE_ENV" \
+    --env APP="$APP" \
+    --network "$NETWORK" \
     --volume "$WEB_PATH":/var/www/example.com/api \
     node:"$VERSION"
