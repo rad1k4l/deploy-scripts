2a8402fffab3732c5117fec7648c4b0a46d15081
Xobolov <farhad.xobolov@gmail.com>
2023-06-08 12:26:23 +0400

    Change mssql deploy scripts

--- mssql/deploy.sh
+++ mssql/deploy.sh
@@ -1,13 +1,17 @@
 echo "load env.."
 . env.sh
 bash ../remove.sh "$CONTAINER_NAME"
-sudo docker run --detach \
+
+docker run --detach \
     --name "$CONTAINER_NAME" \
-    --port 1433:1433 \
-    --env=MSSQL_SA_PASSWORD="$MSSQL_SA_PASSWORD" \
-    --env=MSSQL_PID="$MSSQL_PID" \
-    --env=ACCEPT_EULA="$ACCEPT_EULA" \
-    --network="$NETWORK" \
-    --volume "$DATA_PATH":/var/lib/mssql/data \
+    --env "ACCEPT_EULA=Y" \
+    --env "MSSQL_SA_PASSWORD=$MSSQL_SA_PASSWORD" \
+    --env "MSSQL_PID=$MSSQL_PID" \
+    --network=$NETWORK \
+    --publish $PUBLISH_PORT:1433 \
+    --volume $DATA_PATH:/var/opt/mssql/data \
+    --volume $LOG_PATH:/var/opt/mssql/log \
+    --volume $SECRETS_PATH:/var/opt/mssql/secrets \
     --restart unless-stopped \
-    mcr.microsoft.com/mssql/server:"$VERSION"
+    mcr.microsoft.com/mssql/server:$VERSION
+chmod -R a+rwx "$SERVICE_PATH"
